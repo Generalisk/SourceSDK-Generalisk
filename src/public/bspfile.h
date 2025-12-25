022c8c67f5a19a57720105833bbd60f32b5d1b3b
Generalisk <80116907+Generalisk@users.noreply.github.com>
2025-12-25 23:46:38 +0000

    Extend several internal BSP limits
    Source: https://github.com/ValveSoftware/source-sdk-2013/pull/1194 , https://github.com/ValveSoftware/source-sdk-2013/pull/1176 & https://github.com/ValveSoftware/source-sdk-2013/pull/1564

--- src/public/bspfile.h
+++ src/public/bspfile.h
@@ -57,23 +57,23 @@
 // Common limits
 // leaffaces, leafbrushes, planes, and verts are still bounded by
 // 16 bit short limits
-#define	MAX_MAP_MODELS					1024
-#define	MAX_MAP_BRUSHES					8192
-#define	MAX_MAP_ENTITIES				8192
-#define	MAX_MAP_TEXINFO					12288
-#define MAX_MAP_TEXDATA					2048
-#define MAX_MAP_DISPINFO				2048
+#define	MAX_MAP_MODELS					4096
+#define	MAX_MAP_BRUSHES					65536
+#define	MAX_MAP_ENTITIES				65536
+#define	MAX_MAP_TEXINFO					32768
+#define MAX_MAP_TEXDATA					8192
+#define MAX_MAP_DISPINFO				16384
 #define MAX_MAP_DISP_VERTS				( MAX_MAP_DISPINFO * ((1<<MAX_MAP_DISP_POWER)+1) * ((1<<MAX_MAP_DISP_POWER)+1) )
 #define MAX_MAP_DISP_TRIS				( (1 << MAX_MAP_DISP_POWER) * (1 << MAX_MAP_DISP_POWER) * 2 )
 #define MAX_DISPVERTS					NUM_DISP_POWER_VERTS( MAX_MAP_DISP_POWER )
 #define MAX_DISPTRIS					NUM_DISP_POWER_TRIS( MAX_MAP_DISP_POWER )
-#define	MAX_MAP_AREAS					256
+#define	MAX_MAP_AREAS					1024
 #define MAX_MAP_AREA_BYTES				(MAX_MAP_AREAS/8)
 #define	MAX_MAP_AREAPORTALS				1024
 // Planes come in pairs, thus an even number.
 #define	MAX_MAP_PLANES					65536
 #define	MAX_MAP_NODES					65536
-#define	MAX_MAP_BRUSHSIDES				65536
+#define	MAX_MAP_BRUSHSIDES				655360
 #define	MAX_MAP_LEAFS					65536
 #define	MAX_MAP_VERTS					65536
 #define MAX_MAP_VERTNORMALS				256000
@@ -90,12 +90,14 @@
 #define	MAX_MAP_LIGHTING				0x1000000
 #define	MAX_MAP_VISIBILITY				0x1000000			// increased BSPVERSION 7
 #define	MAX_MAP_TEXTURES				1024
-#define MAX_MAP_WORLDLIGHTS				8192
+#define MAX_MAP_WORLDLIGHTS				65536
 #define MAX_MAP_CUBEMAPSAMPLES			1024
-#define MAX_MAP_OVERLAYS				512 
+#define MAX_MAP_OVERLAYS				8192
 #define MAX_MAP_WATEROVERLAYS			16384
 #define MAX_MAP_TEXDATA_STRING_DATA		256000
 #define MAX_MAP_TEXDATA_STRING_TABLE	65536
+#define MAX_MAP_DETAIL_PROPS			65536	// The object lump is variable-sized, so there's no strict limit.  
+												// The MAX_MAP_DETAIL_PROPS limit is just a warning for the user.
 // this is stuff for trilist/tristrips, etc.
 #define MAX_MAP_PRIMITIVES				32768
 #define MAX_MAP_PRIMVERTS				65536
@@ -784,7 +786,6 @@ struct dfaceid_t
 	DECLARE_BYTESWAP_DATADESC();
 	unsigned short	hammerfaceid;
 };
-
 
 
 // NOTE: Only 7-bits stored!!!
